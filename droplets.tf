692b5a948a37cfc9c18eeab270360f1e80a5da10
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2021-02-17 21:30:55 +0000

    Added 1gb volume to begin with

--- droplets.tf
+++ droplets.tf
@@ -4,3 +4,14 @@ resource "digitalocean_droplet" "docker-1" {
     region = "lon1"
     size = "s-1vcpu-1gb"
 }
+
+resource "digitalocean_volume" "docker-1" {
+    name = "docker-1"
+    size = 1
+    region = "lon1"
+}
+
+resource "digitalocean_volume_attachment" "docker-1" {
+    droplet_id = digitalocean_droplet.docker-1.id
+    volume_id = digitalocean_volume.docker-1.id
+}
