a6eddff89c8f9c62d4af056e2a0ff654228026a1
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2021-11-02 18:53:41 +0000

    Added new droplet and associated volume with it

--- droplets.tf
+++ droplets.tf
@@ -1,17 +1,24 @@
 resource "digitalocean_droplet" "docker-1" {
-    image = "30970148"
-    name = "docker-1"
-    region = "lon1"
-    size = "s-1vcpu-1gb"
+  image  = "30970148"
+  name   = "docker-1"
+  region = "lon1"
+  size   = "s-1vcpu-1gb"
+}
+
+resource "digitalocean_droplet" "docker-2" {
+  image  = "ubuntu-18-04-x64"
+  name   = "docker-2"
+  region = "lon1"
+  size   = "s-1vcpu-1gb"
 }
 
 resource "digitalocean_volume" "docker-1" {
-    name = "docker-1"
-    size = 1
-    region = "lon1"
+  name   = "docker-1"
+  size   = 1
+  region = "lon1"
 }
 
 resource "digitalocean_volume_attachment" "docker-1" {
-    droplet_id = digitalocean_droplet.docker-1.id
-    volume_id = digitalocean_volume.docker-1.id
+  droplet_id = digitalocean_droplet.docker-2.id
+  volume_id  = digitalocean_volume.docker-1.id
 }
