9e48cb926878543d316e97c1799afa33f8b44606
andykuszyk <andy@kuszyk.com>
2023-01-30 22:22:21 +0000

    Imported cronical records

--- cloudflare.tf
+++ cloudflare.tf
@@ -74,3 +74,41 @@ resource "cloudflare_record" "akuszyk_mx1" {
   zone_id  = var.akuszyk_zone_id
 }
 
+resource "cloudflare_record" "cronical_cronical_net" {
+  name    = "cronical.net"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = digitalocean_droplet.docker-2.ipv4_address
+  zone_id = var.cronical_zone_id
+}
+
+resource "cloudflare_record" "cronical_www" {
+  name    = "www"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = digitalocean_droplet.docker-2.ipv4_address
+  zone_id = var.cronical_zone_id
+}
+
+resource "cloudflare_record" "cronical_mx0" {
+  name     = "cronical.net"
+  priority = 10
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "mx0.123-reg.co.uk"
+  zone_id  = var.cronical_zone_id
+}
+
+resource "cloudflare_record" "cronical_mx1" {
+  name     = "cronical.net"
+  priority = 20
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "mx1.123-reg.co.uk"
+  zone_id  = var.cronical_zone_id
+}
+
--- variables.tf
+++ variables.tf
@@ -5,3 +5,7 @@ variable "budjot_zone_id" {
 variable "akuszyk_zone_id" {
   description = "The Cloudflare zone ID for akuszyk.com"
 }
+
+variable "cronical_zone_id" {
+  description = "The Cloudflare zone ID for cronical.net"
+}
